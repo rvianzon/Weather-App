6a73a8d018095992ef67bb5b9fd053bb62186f9f
rvianzon <ronaliza.vianzon@accenture.com>
2022-09-04 20:56:57 +0800

    Initial Commit

---
+++ WeatherApp(new)/WeatherApp_new_App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_new_App.swift
+//  WeatherApp(new)
+//
+//  Created by ronaliza.vianzon on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_new_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
